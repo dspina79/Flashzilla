8e7b0a68695752212efc76ff8ab024fb40920974
Dave Spina <dspina79@gmail.com>
2021-02-09 15:13:55 -0500

    Project initialization

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Dave Spina on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
