56ef79dee6884c1019f0b87ee97546f1c14ba444
Dave Spina <dspina79@gmail.com>
2021-02-10 20:52:35 -0500

    Added hit testing example

--- Flashzilla/Flashzilla/HitTestingExampleView.swift
+++ Flashzilla/Flashzilla/HitTestingExampleView.swift
@@ -7,9 +7,35 @@
 
 import SwiftUI
 
+struct OddShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let myRect = CGRect(x: 0, y: 0, width: 100, height: 100)
+        path.addRect(myRect)
+        return path
+    }
+}
+
 struct HitTestingExampleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .onTapGesture {
+                    print("Rectangle tapped")
+                }
+            
+            Circle()
+                .fill(Color.purple)
+                .contentShape(OddShape())
+                .frame(width: 300, height: 300, alignment:  .center)
+                .onTapGesture {
+                    print("Circle tapped")
+                }
+            
+                // the above will disable the tap event and will go down to the rectangle
+        }
     }
 }
 
