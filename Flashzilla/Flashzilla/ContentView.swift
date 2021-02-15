6c1a97f5cdf2a517b2166240eb190fa230d146af
Dave Spina <dspina79@gmail.com>
2021-02-14 19:31:36 -0500

    Added timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -16,6 +16,10 @@ extension View {
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @State private var isActive = true
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     @State private var cards = [Card](repeating: Card.example, count: 10)
     var body: some View {
         ZStack {
@@ -24,6 +28,13 @@ struct ContentView: View {
                 .scaledToFill()
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
+                Text("Time:  \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(Capsule().fill(Color.black)
+                                    .opacity(0.75))
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) {index in
                         CardView(card: cards[index], removal: {
@@ -55,7 +66,20 @@ struct ContentView: View {
                     .font(.title)
                 }
             }
+        }.onReceive(timer) {timer in
+            guard self.isActive else { return }
+            
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
         }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) {_ in
+            self.isActive = false
+            
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
+            self.isActive = true
+        })
     }
     
     func removeCard(at index: Int) {
